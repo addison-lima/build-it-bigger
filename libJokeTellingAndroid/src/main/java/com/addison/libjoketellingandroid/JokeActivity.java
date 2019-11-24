02c606532c08c04f6788c92176658146f9d2f148
Addison Lima <alima@Addisons-MacBook-Air.local>
2019-11-23 20:59:24 -0400

    create an Android library

---
+++ libJokeTellingAndroid/src/main/java/com/addison/libjoketellingandroid/JokeActivity.java
@@ -0,0 +1,30 @@
+package com.addison.libjoketellingandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static final String INTENT_JOKE = "INTENT_JOKE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        Intent receivedIntent = getIntent();
+
+        if ((receivedIntent != null)
+                && receivedIntent.hasExtra(INTENT_JOKE)) {
+            String joke = receivedIntent.getStringExtra(INTENT_JOKE);
+            TextView jokeTextView = findViewById(R.id.tv_joke);
+            jokeTextView.setText(joke);
+        } else {
+            Toast.makeText(this, getString(R.string.error), Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+}
