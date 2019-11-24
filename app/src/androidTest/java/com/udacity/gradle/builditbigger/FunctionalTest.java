242ff107711f9e6dc83b29b165f8dae675f06c01
Addison Lima <alima@Addisons-MacBook-Air.local>
2019-11-23 22:30:37 -0400

    add functional tests

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/FunctionalTest.java
@@ -0,0 +1,28 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class FunctionalTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void getAJoke_shouldNotHaveEmptyString() {
+        onView(withId(R.id.btn_joke)).perform(click());
+        onView(withId(R.id.tv_joke)).check(matches(not(withText(""))));
+    }
+}
